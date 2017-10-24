f52ded13e711e0da4397b97b325f94301d3af445
Yan Gao <yan.gao@darkmatter.ae>
2017-10-24 09:52:55 -0400

    commit 1

---
+++ src/test/java/RestAPITestExample/GetTest.java
@@ -0,0 +1,27 @@
+package RestAPITestExample;
+
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+
+public class GetTest {
+	@Test
+	  public void RestAPI_Get() {
+		  get("get")
+	      .then()
+	      .body("url", equalTo( "https://httpbin.org/get"))
+		  .body("origin", equalTo("83.110.1.42"));
+	  }
+	  @BeforeTest
+	  public void beforeTest() {
+		  RestAssured.baseURI = "https://httpbin.org/";
+	      //RestAssured.port = 8081;
+		 
+	  }
+
+}
